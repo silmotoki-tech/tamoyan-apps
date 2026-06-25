7aa5abafecbf7ae09ab2248fcf66cec66c6a8259
MTKIN <silmotoki@gmail.com>
2026-06-25 17:28:51 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// tamoyan-apps - ネットワークファースト方式のService Worker
+// 常に最新のファイルをサーバーから取得し、オフラインの時だけキャッシュを使う
+const CACHE_NAME = 'tamoyan-cache-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
